c65236f2cd900c0850d59420669e0f4620c8512d
lucas mateus <lucas.mts1812@gmail.com>
2025-09-12 21:59:45 -0300

    feat: Added DTO for User

---
+++ src/main/java/com/luq/picpay_backend_challenge/dto/request/UserRequest.java
@@ -0,0 +1,12 @@
+package com.luq.picpay_backend_challenge.dto.request;
+
+import java.math.BigDecimal;
+
+public record UserRequest(
+    String fullName,
+    String document,
+    String mail,
+    String password,
+    String confirmPassword,
+    BigDecimal balance
+){}
