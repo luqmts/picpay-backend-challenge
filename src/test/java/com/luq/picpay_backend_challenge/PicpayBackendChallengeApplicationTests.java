89f381165451a82e6eed60ffeb1dea4267eab17c
lucas mateus <lucas.mts1812@gmail.com>
2025-09-13 19:25:23 -0300

    raw: Added config/test default files

---
+++ src/test/java/com/luq/picpay_backend_challenge/PicpayBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luq.picpay_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
